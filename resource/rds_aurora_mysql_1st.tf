d992608ea572aae3b437df397bda2406b6f9e5a0
emilymacpro <attsun1031@gmail.com>
2020-10-31 17:47:48 +0900

    first commit

---
+++ resource/rds_aurora_mysql_1st.tf
@@ -0,0 +1,77 @@
+# aws_rds_cluster_parameter_group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster_parameter_group
+resource "aws_rds_cluster_parameter_group" "aurora_mysql_1st" {
+  name   = "${var.tags_owner}-${var.tags_env}-clspg-aurora-mysql"
+  family = "aurora-mysql5.7"
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+  parameter {
+    name  = "character_set_client"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_connection"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_filesystem"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_results"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "collation_connection"
+    value = "utf8mb4_bin"
+  }
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_bin"
+  }
+  parameter {
+    name  = "time_zone"
+    value = "asia/tokyo"
+  }
+  parameter {
+    name  = "server_audit_events"
+    value = "connect,query"
+  }
+  parameter {
+    name  = "server_audit_excl_users"
+    value = "rdsadmin"
+  }
+  parameter {
+    name  = "server_audit_logging"
+    value = 1
+  }
+  parameter {
+    name  = "server_audit_logs_upload"
+    value = 1
+  }
+  parameter {
+    name  = "slow_query_log"
+    value = 1
+  }
+}
+
+# aws_db_parameter_group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_parameter_group
+resource "aws_db_parameter_group" "aurora_mysql_1st" {
+  name   = "${var.tags_owner}-${var.tags_env}-pg-aurora-mysql"
+  family = "aurora-mysql5.7"
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
