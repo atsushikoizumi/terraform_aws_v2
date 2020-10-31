1e4917d3b8998a70feba09bd50bb6e90d7f68353
emilymacpro <attsun1031@gmail.com>
2020-10-31 16:46:31 +0900

    first commit

---
+++ resource/env/dev/get_tfstate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/resource_dev.tfstate ./
+aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/resource_stg.tfstate ./
+aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/resource_prd.tfstate ./
+aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/vpc.tfstate ./
+
