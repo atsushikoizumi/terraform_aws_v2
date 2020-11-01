1c7b0a4232a56b94cf526eee44d8978a46033ff8
emilymacpro <attsun1031@gmail.com>
2020-11-01 19:13:43 +0900

    aws_iam_policy 追加

--- resource/iam_role_rds_monitoring.tf
+++ resource/iam_role_rds_monitoring.tf
@@ -24,8 +24,46 @@ EOF
   }
 }
 
+# rds_monitoring
+resource "aws_iam_policy" "rds_monitoring" {
+  name = "${var.tags_owner}-${var.tags_env}-policy-rds-monitoring"
+  path = "/"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "EnableCreationAndManagementOfRDSCloudwatchLogGroups",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:PutRetentionPolicy"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:log-group:RDS*"
+            ]
+        },
+        {
+            "Sid": "EnableCreationAndManagementOfRDSCloudwatchLogStreams",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "logs:DescribeLogStreams",
+                "logs:GetLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:log-group:RDS*:log-stream:*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_policy_attachment" "rds_monitoring" {
   name       = "${var.tags_owner}-${var.tags_env}-policy-attachment"
   roles      = [aws_iam_role.rds_monitoring.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+  policy_arn = aws_iam_policy.rds_monitoring.arn
 }
