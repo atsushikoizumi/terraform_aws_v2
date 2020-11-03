13e4da1f5732ea586c1300b6afe58fe301c0fffb
emilymacpro <attsun1031@gmail.com>
2020-11-03 18:02:26 +0900

    Userdataにpython3.8追加

--- resource/ec2_amzn2.tf
+++ resource/ec2_amzn2.tf
@@ -45,6 +45,17 @@ resource "aws_instance" "ec2_amzn2" {
   curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 
+  ### python 3.8
+  yum install -y make gcc zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel libffi-devel xz-devel
+  curl -OL https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tgz
+  tar -xvzf Python-3.8.6.tgz
+  ./Python-3.8.6/configure --prefix=/usr/local/python386 --with-ensurepip
+  make
+  make install
+  ln -s /usr/local/python386/bin/python3 /usr/bin/python3
+  ln -s /usr/local/python386/bin/pip3 /usr/bin/pip3
+  /usr/local/python386/bin/python3.8 -m pip install --upgrade pip
+
   ### awe cli
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/root/awscliv2.zip"
   unzip /root/awscliv2.zip
