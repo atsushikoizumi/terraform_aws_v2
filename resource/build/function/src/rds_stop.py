ed517a58185b3079c164f7c4666b2043e94f5a99
emilymacpro <attsun1031@gmail.com>
2020-11-06 02:33:57 +0900

    add ec2 stop

--- resource/build/function/src/rds_stop.py
+++ resource/build/function/src/rds_stop.py
@@ -74,4 +74,38 @@ def lambda_handler(event, context):
         )
 
 
+    #
+    # ec2 describe_instances/stop_instances
+    #
+    ec2 = boto3.client('ec2')
+    res = ec2.describe_instances()
+
+    # DBClusterIdentifier と Status を一覧で取得
+    for i in range(len(res['Reservations'])):
+
+        # タグ名でフィルター Env=dev, Owner=koizumi なら f=2 で処理継続
+        f = 0
+        for j in range(len(res['Reservations'][i]['Instances'][0]['Tags'])):
+            if str(res['Reservations'][i]['Instances'][0]['Tags'][j]['Key']) == 'Env':
+                if  str(res['Reservations'][i]['Instances'][0]['Tags'][j]['Value']) == 'dev': # os.environ['tags_env']
+                    f += 1
+            if str(res['Reservations'][i]['Instances'][0]['Tags'][j]['Key']) == 'Owner':
+                if  str(res['Reservations'][i]['Instances'][0]['Tags'][j]['Value']) == 'koizumi': # os.environ['tags_owner']
+                    f += 1
+            if str(res['Reservations'][i]['Instances'][0]['Tags'][j]['Key']) == 'Name':
+                # ec2 instance の状態出力
+                print(res['Reservations'][i]['Instances'][0]['Tags'][j]['Value'] + '  ' + res['Reservations'][i]['Instances'][0]['State']['Name'])
+                if  str(res['Reservations'][i]['Instances'][0]['Tags'][j]['Value']) == 'koizumi-dev-win2019': # os.environ['ec2_win_name']
+                    f += 1
+        if f != 3:
+            continue
+
+        # State = running は処理
+        if str(res['Reservations'][i]['Instances'][0]['State']['Name']) != 'running':
+            continue
+
+        # db cluster を停止
+        ec2.stop_instances(
+            InstanceIds = [res['Reservations'][i]['Instances'][0]['InstanceId']]
+        )
 
