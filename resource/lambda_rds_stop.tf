1d569991f1a83362c81ac22d790ba60ffc430717
emilymacpro <attsun1031@gmail.com>
2020-11-06 01:09:47 +0900

    ソース準備方法を記載

--- resource/lambda_rds_stop.tf
+++ resource/lambda_rds_stop.tf
@@ -1,8 +1,13 @@
 #
 # 参考：https://dev.classmethod.jp/articles/terraform-lambda-deployment/
 #      https://qiita.com/ktsujichan/items/c0804f155c2cf1962ed3
+# 
+# ソースの準備方法
+#   1. build/function/src 配下に python3.8 実行プログラム配置
+#   2. python3 -m pip install boto3 -t build/layer/python  パッケージ取得
+#   3. build/function --> lambda/function.zip
+#   4. build/layer/python --> lambda/python.zip
 #
-
 
 # Layer
 resource "aws_lambda_layer_version" "rds_stop" {
