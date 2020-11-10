cde7b16fffa5a9c5caaa7c1f4507672d3304bdba
emilymacpro <attsun1031@gmail.com>
2020-11-10 11:42:55 +0900

    isid global を追加

--- resource/env/dev/get_tfstate.sh
+++ resource/env/dev/get_tfstate.sh
@@ -2,6 +2,7 @@
 
 aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/resource_dev.tfstate ./
 aws s3 cp s3://aws-aqua-terraform/koizumi/isid/resources.tfstate ./
+aws s3 cp s3://aws-aqua-terraform/koizumi/isid/global.tfstate ./
 #aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/resource_stg.tfstate ./
 #aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/resource_prd.tfstate ./
 #aws s3 cp s3://aws-aqua-terraform/koizumi/dba-test/vpc.tfstate ./
