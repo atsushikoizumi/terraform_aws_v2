82044f17989dd2750a73dc6aa00e6cf652736941
emilymacpro <attsun1031@gmail.com>
2020-11-10 12:03:14 +0900

    パスワード追加

--- resource/secrets_manager.tf
+++ resource/secrets_manager.tf
@@ -4,19 +4,22 @@
 # アンダーバーにしておくのが無難。
 #
 resource "aws_secretsmanager_secret" "aurora_pass" {
-  name = "${var.tags_owner}_${var.tags_env}_aurora_pass"
+  name                    = "${var.tags_owner}_${var.tags_env}_aurora_pass"
   recovery_window_in_days = 7
-  description = "rds aurora logical backup"
-    tags = {
+  description             = "rds aurora logical backup"
+  tags = {
     Owner = var.tags_owner
     Env   = var.tags_env
   }
 }
 
-variable "aurora_pass" {
+variable "db_master_password" {
   default = {
     "postgresql" = "Admin123!"
-    "mysql" = "Admin123!"
+    "mysql"      = "Admin123!"
+    "oracle"     = "Admin123!"
+    "sqlserver"  = "Admin123!"
+    "redshift"   = "Admin123!"
   }
 
   type = map(string)
@@ -24,5 +27,5 @@ variable "aurora_pass" {
 
 resource "aws_secretsmanager_secret_version" "aurora_pass" {
   secret_id     = aws_secretsmanager_secret.aurora_pass.id
-  secret_string = jsonencode(var.aurora_pass)
+  secret_string = jsonencode(var.db_master_password)
 }
