01dafc518dff628b0a77b69e461acb32b89ac847
emilymacpro <attsun1031@gmail.com>
2020-11-11 19:20:32 +0900

    fargate タスクスケジュール追加

--- resource/cloudwatch_event.tf
+++ resource/cloudwatch_event.tf
@@ -1,4 +1,4 @@
-# event schedule
+# resource_stop schedule
 resource "aws_cloudwatch_event_rule" "resource_stop" {
   name                = "${var.tags_owner}-${var.tags_env}-resource-stop"
   description         = "resource stop schedule"
@@ -14,4 +14,40 @@ resource "aws_cloudwatch_event_target" "resource_stop" {
   rule      = aws_cloudwatch_event_rule.resource_stop.name
   target_id = "${var.tags_owner}-${var.tags_env}-resource-stop"
   arn       = aws_lambda_function.resource_stop.arn
+  role_arn = ""
+  input = "{}"
+}
+
+# logicalbackup schedule
+resource "aws_cloudwatch_event_rule" "logicalbackup" {
+  name                = "${var.tags_owner}-${var.tags_env}-logicalbackup"
+  description         = "rds logicalbackup schedule"
+  schedule_expression = "cron(0 2 * * ? *)"
+  is_enabled          = true
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
+resource "aws_cloudwatch_event_target" "logicalbackup" {
+  rule = aws_cloudwatch_event_rule.logicalbackup.name
+  target_id = "${var.tags_owner}-${var.tags_env}-logicalbackup-task"
+  arn = aws_ecs_cluster.logicalbackup.arn
+  role_arn = aws_iam_role.cloudwatch_events_role.arn
+  input = "{}"
+
+  ecs_target {
+    task_count = 1
+    task_definition_arn = aws_ecs_task_definition.logicalbackup.arn
+    launch_type = "FARGATE"
+    platform_version = "1.4.0"  # 1.4 ではなく、 1.4.0 と指定しないと動かない
+
+    network_configuration {
+      assign_public_ip = true
+      security_groups = [aws_security_group.ec2.id]
+      subnets = [aws_subnet.ec2["eu-north-1a"].id]
+    }
+  }
+  
 }
---
+++ resource/iam_role_exec_ecs_task.tf
@@ -0,0 +1,147 @@
+# [参考]
+# https://github.com/turnerlabs/terraform-ecs-fargate-scheduled-task/blob/master/env/dev/ecs.tf
+#
+# ecsTaskExecutionRole:   AssumeRole = ecs-tasks.amazonaws.com
+#                         policy     = AmazonECSTaskExecutionRolePolicy
+# cloudwatch_events_role: AssumeRole = events.amazonaws.com
+#                         policy     = ecs:RunTask to ecs-task
+#                         passrole   = ec2_role,ecsTaskExecutionRole
+#
+#
+
+# ecsTaskExecutionRole
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "${var.tags_owner}-${var.tags_env}-role-ecs"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "ecs-tasks.amazonaws.com"
+        ]
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# allow task execution role to worl with secret manager
+resource "aws_iam_policy" "exec_task_1" {
+  name = "${var.tags_owner}-${var.tags_env}-policy-exec-task-1"
+  path = "/"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "secretsmanager:GetSecretValue",
+                "kms:Decrypt"
+            ],
+            "Resource": "*"
+        } 
+    ]
+}
+EOF
+}
+resource "aws_iam_policy_attachment" "exec_task_1" {
+  name = "${var.tags_owner}-${var.tags_env}-exec-task-1"
+  roles = [aws_iam_role.ecsTaskExecutionRole.name]
+  policy_arn = aws_iam_policy.exec_task_1.arn
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/CWE_IAM_role.html
+resource "aws_iam_role" "cloudwatch_events_role" {
+  name = "${var.tags_owner}-${var.tags_env}-role-events"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "events.amazonaws.com"
+        ]
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# allow events role to run ecs tasks
+resource "aws_iam_policy" "exec_task_2" {
+  name = "${var.tags_owner}-${var.tags_env}-policy-exec-task-2"
+  path = "/"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecs:RunTask"
+            ],
+            "Resource": [
+                "${aws_ecs_task_definition.logicalbackup.arn}"
+            ]
+        } 
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "exec_task_2" {
+  name = "${var.tags_owner}-${var.tags_env}-exec-task-2"
+  roles = [aws_iam_role.cloudwatch_events_role.name]
+  policy_arn = aws_iam_policy.exec_task_2.arn
+}
+
+# allow events role to pass role to task execution role and app role
+resource "aws_iam_policy" "exec_task_3" {
+  name = "${var.tags_owner}-${var.tags_env}-policy-exec-task-3"
+  path = "/"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:PassRole"
+            ],
+            "Resource": [
+                "${aws_iam_role.ec2.arn}",
+                "${aws_iam_role.ecsTaskExecutionRole.arn}"
+            ],
+            "Condition": {
+                "StringLike": {
+                    "iam:PassedToService": "ecs-tasks.amazonaws.com"
+                }
+            }
+        } 
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "exec_task_3" {
+  name = "${var.tags_owner}-${var.tags_env}-exec-task-3"
+  roles = [aws_iam_role.cloudwatch_events_role.name]
+  policy_arn = aws_iam_policy.exec_task_3.arn
+}
