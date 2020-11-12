bdf4a8f0a4733e698b5d9d033620551361386463
emilymacpro <attsun1031@gmail.com>
2020-11-12 21:06:45 +0900

    cron 修正

--- resource/cloudwatch_event.tf
+++ resource/cloudwatch_event.tf
@@ -2,7 +2,7 @@
 resource "aws_cloudwatch_event_rule" "resource_stop" {
   name                = "${var.tags_owner}-${var.tags_env}-resource-stop"
   description         = "resource stop schedule"
-  schedule_expression = "cron(0 */2 * * ? *)"
+  schedule_expression = "cron(0 */2 * * ? *)"  # UTC
   is_enabled          = var.resource_stop_flag
   tags = {
     Owner = var.tags_owner
@@ -18,11 +18,31 @@ resource "aws_cloudwatch_event_target" "resource_stop" {
   input = "{}"
 }
 
+# resource_start schedule
+resource "aws_cloudwatch_event_rule" "resource_start" {
+  name                = "${var.tags_owner}-${var.tags_env}-resource-start"
+  description         = "resource start schedule"
+  schedule_expression = "cron(30 6 * * ? *)"  # UTC
+  is_enabled          = true
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
+resource "aws_cloudwatch_event_target" "resource_start" {
+  rule      = aws_cloudwatch_event_rule.resource_start.name
+  target_id = "${var.tags_owner}-${var.tags_env}-resource-start"
+  arn       = aws_lambda_function.resource_start.arn
+  role_arn = ""
+  input = "{}"
+}
+
 # logicalbackup schedule
 resource "aws_cloudwatch_event_rule" "logicalbackup" {
   name                = "${var.tags_owner}-${var.tags_env}-logicalbackup"
   description         = "rds logicalbackup schedule"
-  schedule_expression = "cron(*/10 * * * ? *)"
+  schedule_expression = "cron(30 7 * * ? *)"  # UTC
   is_enabled          = true
   tags = {
     Owner = var.tags_owner
