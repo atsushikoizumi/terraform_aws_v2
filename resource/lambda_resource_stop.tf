bead9f8baf0229ebadcd170318d9e8703924f36b
emilymacpro <attsun1031@gmail.com>
2020-11-12 12:13:40 +0900

    ビルドを terraform 化

--- resource/lambda_resource_stop.tf
+++ resource/lambda_resource_stop.tf
@@ -3,23 +3,43 @@
 #      https://qiita.com/ktsujichan/items/c0804f155c2cf1962ed3
 # 
 # ソースの準備方法
-#   1. build/function/src 配下に python3.8 実行プログラム配置
-#   2. パッケージ取得
-#       cd resource
-#       python3 -m pip install boto3 -t build/layer/python
-#       python3 -m pip install datetime -t build/layer/python
-#   3. 圧縮
-#       build/function/src --> build/function/src.zip
-#       build/layer/python --> build/layer/python.zip
-#   4. 配置
-#       build/function/src.zip --> build/lambda/src.zip
-#       build/layer/python.zip --> build/lambda/python.zip
+#   1. build/resource_stop/function/src 配下に python3.8 実行プログラム配置
+#   2. ディレクトリ（resource）に移動
+#       cd ~/.../resource
+#   3. python ライブラリ保存用ディレクトリ作成
+#       mkdir -p build/resource_stop/layer/python
+#   4. ライブラリのインストール
+#       python3 -m pip install boto3    -t build/resource_stop/layer/python
+#       python3 -m pip install datetime -t build/resource_stop/layer/python
+#   5. キャッシュ系ファイルの削除（不要な差分回避のため）
+#       find build -type f | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm
+#
+
+
+# Archive
+data "archive_file" "layer_zip" {
+  type        = "zip"
+  source_dir  = "../../build/resource_stop/layer"
+  output_path = "../../build/resource_stop/layer.zip"
+}
+
+data "archive_file" "function_zip" {
+  type        = "zip"
+  source_dir  = "../../build/resource_stop/function"
+  output_path = "../../build/resource_stop/function.zip"
+}
 
 # Layer
 resource "aws_lambda_layer_version" "resource_stop" {
   layer_name       = "${var.tags_owner}-${var.tags_env}-resource-stop"
-  filename         = var.layer_zip
-  source_code_hash = filebase64sha256(var.layer_zip)
+  filename         = data.archive_file.layer_zip.output_path
+  source_code_hash = filebase64sha256(data.archive_file.layer_zip.output_path)
+  # ソースコードのハッシュ値で変更の有無を判断するため、日付は無視する
+  lifecycle {
+    ignore_changes = [
+      created_date
+    ]
+  }
 }
 
 # Function
@@ -27,14 +47,19 @@ resource "aws_lambda_function" "resource_stop" {
   function_name = "${var.tags_owner}-${var.tags_env}-resource-stop"
 
   handler          = "src/resource_stop.lambda_handler"
-  filename         = var.function_zip
+  filename         = data.archive_file.function_zip.output_path
   runtime          = "python3.8"
   publish          = true
   timeout          = 10
   role             = aws_iam_role.lambda.arn
   layers           = [aws_lambda_layer_version.resource_stop.arn]
-  source_code_hash = filebase64sha256(var.function_zip)
-
+  source_code_hash = filebase64sha256(data.archive_file.function_zip.output_path)
+  # ソースコードのハッシュ値で変更の有無を判断するため、日付は無視する
+  lifecycle {
+    ignore_changes = [
+      last_modified
+    ]
+  }
   environment {
     variables = {
       tags_owner   = var.tags_owner
