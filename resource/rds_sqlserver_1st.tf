99ce03b46f49c548507243cd3b14a4daa699cd5c
emilymacpro <attsun1031@gmail.com>
2020-12-25 09:10:16 +0900

    パラメータグループファイルは削除

--- resource/rds_sqlserver_1st.tf
+++ resource/rds_sqlserver_1st.tf
@@ -1,3 +1,60 @@
+# aws_db_parameter_group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_parameter_group
+resource "aws_db_parameter_group" "sqlserver_1st" {
+  name   = "${var.tags_owner}-${var.tags_env}-pg-sqlserver-1st"
+  family = "sqlserver-se-14.0"
+
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
+# aws_db_option_group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_option_group
+resource "aws_db_option_group" "sqlserver_1st" {
+  name                 = "${var.tags_owner}-${var.tags_env}-opg-sqlserver-1st"
+  engine_name          = "sqlserver-se"
+  major_engine_version = "14.00"
+
+  option {
+    option_name = "SQLSERVER_BACKUP_RESTORE"
+
+    option_settings {
+      name  = "IAM_ROLE_ARN"
+      value = aws_iam_role.rds.arn
+    }
+  }
+
+  option {
+    option_name = "SQLSERVER_AUDIT"
+
+    option_settings {
+      name  = "ENABLE_COMPRESSION"
+      value = true
+    }
+    option_settings {
+      name  = "S3_BUCKET_ARN"
+      value = "arn:aws:s3:::${var.tags_owner}-${var.tags_env}-logs/audit/"
+    }
+    option_settings {
+      name  = "IAM_ROLE_ARN"
+      value = aws_iam_role.rds.arn
+    }
+    option_settings {
+      name  = "RETENTION_TIME"
+      value = 0
+    }
+
+  }
+
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
+
 # aws_db_instance
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/db_instance
 resource "aws_db_instance" "sqlserver_1st" {
