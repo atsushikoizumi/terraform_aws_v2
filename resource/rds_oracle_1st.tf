6eda5599c9a2e4f80b60f1eba2b27c1da6e13be8
emilymacpro <attsun1031@gmail.com>
2021-02-10 17:53:19 +0900

    add vup用ファイル

--- resource/rds_oracle_1st.tf
+++ resource/rds_oracle_1st.tf
@@ -29,6 +29,65 @@ resource "aws_db_parameter_group" "oracle_1st" {
   }
 }
 
+resource "aws_db_parameter_group" "oracle_18" {
+  name   = "${var.tags_owner}-${var.tags_env}-pg-oracle-18"
+  family = "oracle-se2-18"
+
+  parameter {
+    name         = "audit_trail"
+    value        = "XML,EXTENDED"
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "audit_sys_operations"
+    value        = false
+    apply_method = "pending-reboot"
+  }
+
+  # lifecycle
+  lifecycle {
+    ignore_changes = [
+      parameter
+    ]
+  }
+
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
+
+resource "aws_db_parameter_group" "oracle_121se2" {
+  name   = "${var.tags_owner}-${var.tags_env}-pg-oracle-121se2"
+  family = "oracle-se2-12.1"
+
+  parameter {
+    name         = "audit_trail"
+    value        = "XML,EXTENDED"
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "audit_sys_operations"
+    value        = false
+    apply_method = "pending-reboot"
+  }
+
+  # lifecycle
+  lifecycle {
+    ignore_changes = [
+      parameter
+    ]
+  }
+
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
 # aws_db_option_group
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_option_group
 resource "aws_db_option_group" "oracle_1st" {
@@ -62,6 +121,67 @@ resource "aws_db_option_group" "oracle_1st" {
   }
 }
 
+resource "aws_db_option_group" "oracle_18" {
+  name                 = "${var.tags_owner}-${var.tags_env}-opg-oracle-18"
+  engine_name          = "oracle-se2"
+  major_engine_version = 18
+
+  option {
+    option_name                    = "S3_INTEGRATION"
+    db_security_group_memberships  = []
+    port                           = 0
+    version                        = "1.0"
+    vpc_security_group_memberships = []
+  }
+
+  option {
+    option_name                    = "Timezone"
+    db_security_group_memberships  = []
+    port                           = 0
+    vpc_security_group_memberships = []
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Asia/Tokyo"
+    }
+  }
+
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
+resource "aws_db_option_group" "oracle_121se2" {
+  name                 = "${var.tags_owner}-${var.tags_env}-opg-oracle-121se2"
+  engine_name          = "oracle-se2"
+  major_engine_version = 12.1
+
+  option {
+    option_name                    = "S3_INTEGRATION"
+    db_security_group_memberships  = []
+    port                           = 0
+    version                        = "1.0"
+    vpc_security_group_memberships = []
+  }
+
+  option {
+    option_name                    = "Timezone"
+    db_security_group_memberships  = []
+    port                           = 0
+    vpc_security_group_memberships = []
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Asia/Tokyo"
+    }
+  }
+
+  tags = {
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
 
 # aws_db_instance
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/db_instance
