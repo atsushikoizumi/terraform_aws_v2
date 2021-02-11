0341765ab1fa3ac422cea5d887ee38c637f3671e
emilymacpro <attsun1031@gmail.com>
2021-02-11 23:23:51 +0900

    add s3export role

--- resource/iam_role_rds.tf
+++ resource/iam_role_rds.tf
@@ -69,3 +69,76 @@ resource "aws_iam_policy_attachment" "rds_1" {
   roles      = [aws_iam_role.rds.name]
   policy_arn = aws_iam_policy.rds_1.arn
 }
+
+
+# AssumeRole
+resource "aws_iam_role" "rds2" {
+  name = "${var.tags_owner}-${var.tags_env}-role-rds2"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "rds.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name  = "${var.tags_owner}-${var.tags_env}-role-rds2"
+    Owner = var.tags_owner
+    Env   = var.tags_env
+  }
+}
+
+# s3
+resource "aws_iam_policy" "rds_2" {
+  name = "${var.tags_owner}-${var.tags_env}-policy-rds-2"
+  path = "/"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+	      {
+	          "Effect": "Allow",
+	          "Action": "s3:ListAllMyBuckets",
+	          "Resource": "*"
+	      },
+        {
+            "Sid": "s3export",
+            "Effect": "Allow",
+            "Action": [
+                "s3:AbortMultipartUpload",
+                "s3:DeleteObject",
+                "S3:GetBucketAcl",
+                "s3:GetBucketLocation",
+                "s3:GetObject",
+                "s3:GetObjectMetaData",
+                "s3:ListBucket",
+                "s3:ListMultipartUploadParts",
+                "s3:PutObject"
+            ],
+            "Resource": [
+              "arn:aws:s3:::${var.tags_owner}-${var.tags_env}-logs",
+              "arn:aws:s3:::${var.tags_owner}-${var.tags_env}-logs/*",
+              "arn:aws:s3:::${var.tags_owner}-${var.tags_env}-data",
+              "arn:aws:s3:::${var.tags_owner}-${var.tags_env}-data/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "rds_2" {
+  name       = "${var.tags_owner}-${var.tags_env}-policy-attachment2"
+  roles      = [aws_iam_role.rds2.name]
+  policy_arn = aws_iam_policy.rds_2.arn
+}
